a78bd4f53ebefa2f728def83909620ae26d9691d
Benjamin Meng <benjaminmeng1994@gmail.com>
2022-10-28 13:26:28 -0700

    class examples completed

--- classExample.js
+++ classExample.js
@@ -1,5 +1,31 @@
 class Pizza {
-  constructor() {
+  constructor(size, crust) {
+    this.size = size;
+    this.crust = crust;
     this.toppings  = ["cheese"];
   }
-}
+
+  addTopping(topping) {
+    this.toppings.push(topping);
+  }
+
+  addFooter() {
+    console.log("-------------------");
+  }
+};
+
+let pizza1 = new Pizza('large', 'thin');
+console.log(pizza1.toppings);
+pizza1.addTopping("mushrooms");
+pizza1.addTopping("peppers");
+console.log(pizza1.toppings);
+console.log(pizza1);
+pizza1.addFooter();
+
+
+let pizza2 = new Pizza('small', 'thick');
+
+console.log(pizza2.toppings);
+pizza2.addTopping("more cheese");
+console.log(pizza2.toppings);
+console.log(pizza2);
